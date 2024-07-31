1df34ed796f81f8ab5ad4c24ef27c56524cd3fca
Connor Nguyen <nguyendinhthanhcong2512@gmail.com>
2024-07-30 18:26:27 -0700

    First Commit

---
+++ EdutainmentApp/EdutainmentAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdutainmentAppApp.swift
+//  EdutainmentApp
+//
+//  Created by Apple on 30/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct EdutainmentAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
