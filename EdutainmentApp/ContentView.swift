0161302a1508aea5f2a8bd2f8bbcc2b8b647494d
Connor Nguyen <nguyendinhthanhcong2512@gmail.com>
2024-07-31 12:42:41 -0700

    Almost complete the Calculation game

--- EdutainmentApp/ContentView.swift
+++ EdutainmentApp/ContentView.swift
@@ -14,7 +14,15 @@ struct ContentView: View {
     let step = 5
     let listOfNumbers = [2, 3, 4, 5, 6, 7, 8, 9 ,10, 11, 12]
     @State private var counting = 0
-    @State private var listOfAnswers = []
+    @State private var listOfAnswers = [Int]()
+    @State private var number1 = 0
+    @State private var number2 = 0
+    @State private var answer1 = 0
+    @State private var answer2 = 0
+    @State private var answer = 0
+    @State private var scoreTittle = ""
+    @State private var score = 0
+    @State private var showingScore = false
     
     var body: some View {
         ZStack{
@@ -27,7 +35,7 @@ struct ContentView: View {
                     .padding(5)
                 
                 Section{
-                    Stepper("Enter your range from 2-12 \nCurrent Range: \(range)", value: $range,
+                    Stepper("Enter your range from 2-12 \nCurrent Range: \(String(range + 1))", value: $range,
                             in: 2...12 )
                     .font(.subheadline.bold())
                     
@@ -44,15 +52,9 @@ struct ContentView: View {
                 Spacer()
             }
             .padding()
+            .onAppear(perform: generateQuestions )
             
             VStack{
-                
-                let number1 = listOfNumbers[Int.random(in: 0..<range)]
-                let number2 = listOfNumbers[Int.random(in: 0..<range)]
-                let answer1 = Int.random(in: 2..<144)
-                let answer2 = Int.random(in: 2..<144)
-                let answer = number1 * number2
-                
                 HStack {
                     Text("\(number1)")
                     
@@ -62,34 +64,63 @@ struct ContentView: View {
                 }
                 .font(.system(size: 100))
                 
+                
                 HStack{ //Stack for 3 numbers of answers
-                    Text("\(answer1)")
-                        .frame(width: 100, height: 100)
-                        .foregroundColor(.white)
-                        .background(Color.secondary)
-                        .cornerRadius(20)
-                    
-                    Text("\(answer)")
-                        .frame(width: 100, height: 100)
-                        .foregroundColor(.white)
-                        .background(Color.secondary)
-                        .cornerRadius(20)
-                    
-                    Text("\(answer2)")
-                        .frame(width: 100, height: 100)
-                        .foregroundColor(.white)
-                        .background(Color.secondary)
-                        .cornerRadius(20)
+                    ForEach(listOfAnswers.shuffled(), id: \.self) { answer3 in
+                        Button(action: {
+                            checkAnswer(answer3)
+                            print("Button tapped: \(answer3)")
+                        }) {
+                            withAnimation{
+                                Text(String(answer3))
+                                    .frame(width: 100, height: 100)
+                                    .foregroundColor(.white)
+                                    .background(Color.secondary)
+                                    .cornerRadius(20)
+                            }
+                        }
+                    }
                 }
             }
         }
+        .alert(scoreTittle, isPresented: $showingScore){
+            Button("Try again?", action: generateQuestions)
+        } message: {
+            Text("Your score is \(score)")
+        }
     }
+    
+    func addAnswerToArray(answer1: Int, answer2: Int, correctAnswer: Int) {
+        listOfAnswers = [answer1, answer2, correctAnswer]
+    }
+    
+    func generateQuestions() {
+        number1 = listOfNumbers[Int.random(in: 0..<range)]
+        number2 = listOfNumbers[Int.random(in: 0..<range)]
+        answer1 = Int.random(in: 2..<144)
+        answer2 = Int.random(in: 2..<144)
+        answer = number1 * number2
+        
+        addAnswerToArray(answer1: answer1, answer2: answer2, correctAnswer: answer)
+    }
+    
+    func checkAnswer(_ number: Int){
+        if number == answer {
+            scoreTittle = "Correct Answer"
+            score += 1
+            counting += 1
+        } else {
+            scoreTittle = "Wrong Answer"
+            score = 0
+        }
+        if counting == question_Num{
+            scoreTittle = "You have completed all the questions"
+            score = 0
+        }
+        showingScore = true
+    }
+    
 }
-
-
-
-
-
 
 
 
